e68dfac1147a1c33d3d8a4bae96fd24d4ad142f1
lúa medina <luamedina.dev@gmail.com>
2025-05-13 20:46:30 -0300

    chore: create FileEntry type

--- src/components/Launcher.tsx
+++ src/components/Launcher.tsx
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import { readDir } from '@tauri-apps/plugin-fs';
 import { homeDir } from '@tauri-apps/api/path';
 
+type FileEntry = {
+    name: string,
+    path: string,
+    children?: FileEntry[];
+};
 function Launcher() {
 
     return (
