e88bafc795e1271806361331c44c828698b66755
lúa medina <luamedina.dev@gmail.com>
2025-05-15 17:40:24 -0300

    chore: import useEffect

--- src/components/Launcher.tsx
+++ src/components/Launcher.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { readDir } from '@tauri-apps/plugin-fs';
 import { homeDir } from '@tauri-apps/api/path';
 
